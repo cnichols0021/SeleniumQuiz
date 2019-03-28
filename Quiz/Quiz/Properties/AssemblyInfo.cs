f86779b57ebd51444c121351c8def0c97d381a01
Charlie Nichols <cnichols0021@gmail.com>
2019-03-27 21:57:36 -0400

    Initial commit

---
+++ Quiz/Quiz/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quiz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quiz")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e73f739-e1bd-448d-869c-cce07da8ce74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Quiz/Quiz/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Quiz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ultimateqa.com/filling-out-forms/");
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.Id("et_pb_contact_name_1")).SendKeys("Quiz Time");
+            driver.FindElement(By.Id("et_pb_contact_message_1")).SendKeys("Quiz Passed!!!");
+            string digits = driver.FindElement(By.ClassName("et_pb_contact_captcha_question")).Text;
+            string firstDigit = driver.FindElement(By.XPath("//input[@data-first_digit]")).GetAttribute("data-first_digit");
+            string secondDigit = driver.FindElement(By.XPath("//input[@data-second_digit]")).GetAttribute("data-second_digit");
+            Console.WriteLine(digits);
+            Console.WriteLine(firstDigit);
+            Console.WriteLine(secondDigit);
+            int sum = Int32.Parse(firstDigit) + Int32.Parse(secondDigit);
+            Console.WriteLine(sum);
+            driver.FindElement(By.Name("et_pb_contact_captcha_1")).SendKeys(sum.ToString());
+            driver.FindElement(By.XPath("(//button[contains(text(),'Submit')])[2]")).Click();
+            driver.Quit();
+        }
+    }
+}
